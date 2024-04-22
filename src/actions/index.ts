ebd4b5c8137e7d967ff698ef98fdc26f43df5b89
Belén Albeza <belen@hey.com>
2024-04-22 18:29:39 +0200

    Step 4: Remove coupling in AttackAction

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,28 @@
+import Vec2d from "../utils/vec2d";
+
 export { default as HealAction } from "./heal";
+export { default as AttackAction } from "./attack";
 
 interface HasHealth {
   health: number;
   isAlive: boolean;
 }
+
+interface HasLevel {
+  level: number;
+}
+
+interface HasPosition {
+  position: Vec2d;
+}
+
 export interface Healer extends HasHealth {
   healing: number;
 }
+
+export interface Attacker extends HasHealth, HasLevel, HasPosition {
+  damage: number;
+  range: number;
+}
+
+export interface AttackTarget extends HasHealth, HasLevel, HasPosition {}
