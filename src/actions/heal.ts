a5ffc05fa7141296fe1372ff4c530ab03c9f4552
Belén Albeza <belen@hey.com>
2024-05-18 08:30:32 +0200

    Step 5: Add HealAnother action

--- src/actions/heal.ts
+++ src/actions/heal.ts
@@ -1,6 +1,6 @@
-import { Healer } from ".";
+import { AllianceInformer, Healer, HealTarget } from ".";
 
-export default class HealAction {
+export class HealAction {
   readonly source: Healer;
 
   constructor(source: Healer) {
@@ -13,5 +13,33 @@ export default class HealAction {
     }
 
     this.source.health += this.source.healing;
+  }
+}
+
+export class HealAnotherAction {
+  readonly source: Healer;
+  readonly target: HealTarget;
+  #alliances: AllianceInformer;
+
+  constructor(source: Healer, target: HealTarget, alliances: AllianceInformer) {
+    this.source = source;
+    this.target = target;
+    this.#alliances = alliances;
+  }
+
+  run() {
+    if (!this.source.isAlive) {
+      throw new Error("Dead characters cannot heal");
+    }
+
+    if (!this.target.isAlive) {
+      throw new Error("Invalid heal target (cannot heal dead characters)");
+    }
+
+    if (!this.#alliances.areAllies(this.source, this.target)) {
+      throw new Error("Invalid heal target (can only heal allies)");
+    }
+
+    this.target.health += this.source.healing;
   }
 }
